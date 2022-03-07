4352d3b96b8006c1006093e3b3751cc74104c22a
OshriTzagay <oshritzagay@gmail.com>
2022-03-07 10:33:35 +0200

    Removed Env

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: " AIzaSyDiJ3EPn42ybEOiJi8iu0wdYWl2RUlmuTE ",
   authDomain: "my-first-firebase-app-d49df.firebaseapp.com",
   projectId: "my-first-firebase-app-d49df",
   storageBucket: "my-first-firebase-app-d49df.appspot.com",
   messagingSenderId: "347217348918",
-  appId: "1:347217348918:web:c01506dba6eb0dc0014b97"
+  appId: "1:347217348918:web:c01506dba6eb0dc0014b97",
 };
 export default firebaseConfig;
 
